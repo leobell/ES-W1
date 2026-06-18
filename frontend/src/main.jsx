dae89cc5605aa3b8078fad3ced1f0937559cf0d4
leobell <bellleonardo04@gmail.com>
2026-06-18 19:44:06 +0200

    adding backend and frontend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+//import reportWebVitals from "./reportWebVitals";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
